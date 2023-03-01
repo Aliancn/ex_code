4c2cdbf3be7d02476bde66d7c2080db838ec1e5d
chuseng <2022300004054@whu.edu.cn>
2023-03-01 14:17:48 +0000

    Init push

---
+++ Code/Init.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include<iostream>
+using namespace std;
+int main(){
+    freopen("Code/in.in","r" ,stdin);
+    freopen("Code/out.out","w",stdout);
+    int a;
+    cin>>a;
+    cout<<a<<endl;
+    cout<<"hello world"<<endl;
+    return 0;
+}
